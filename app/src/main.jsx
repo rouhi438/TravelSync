68335e4845b9718e18d69bc33fad6d062daa75ed
Dimitar Dimitrov <d_iwanow@outlook.com>
2026-06-16 23:33:05 +0200

    chore: run `npm run format`

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -9,5 +9,5 @@ import "./main.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
