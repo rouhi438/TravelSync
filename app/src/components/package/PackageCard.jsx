573ba45576a42016a75eed11aba9b43bbbf605c8
Abbas Rouhi Hansen <rouhi438@gmail.com>
2026-06-22 23:26:17 +0200

    add package rating feature (#23)

--- app/src/components/package/PackageCard.jsx
+++ app/src/components/package/PackageCard.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+import { Rating } from "../rating/Rating";
+import { getRatingStats } from "../../utils/ratingUtils";
 import { useWishlist } from "../../context/WishlistContext.jsx";
 import "./PackageCard.css";
-import { Link } from "react-router-dom";
+
 export default function PackageCard({ package: pkg }) {
+  const { avg: avgRating, count: reviewCount } = getRatingStats(pkg.ratings);
   const { isInWishlist, toggleWishlist } = useWishlist();
   const inWishlist = isInWishlist(pkg.id);
 
@@ -15,6 +19,10 @@ export default function PackageCard({ package: pkg }) {
         <div className="price-row">
           <span className="price">€ {pkg.price}</span>
           <span className="duration">{pkg.duration}</span>
+        </div>
+
+        <div className="rating-section">
+          <Rating rating={avgRating} reviewCount={reviewCount} />
         </div>
 
         <button
