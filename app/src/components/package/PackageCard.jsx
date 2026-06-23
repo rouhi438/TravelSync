1003faed627f416d45dd122faf65105d71f95661
Iglesia Desta <iglesiad25@gmail.com>
2026-06-23 19:47:42 +0200

    packagecard.jsx saved

--- app/src/components/package/PackageCard.jsx
+++ app/src/components/package/PackageCard.jsx
@@ -1,22 +1,14 @@
 import { Link } from "react-router-dom";
-import { Rating } from "../rating/Rating";
-import { getRatingStats } from "../../utils/ratingUtils";
 import { useWishlist } from "../../context/WishlistContext.jsx";
 import "./PackageCard.css";
 
 export default function PackageCard({ package: pkg }) {
-  const { avg: avgRating, count: reviewCount } = getRatingStats(pkg.ratings);
   const { isInWishlist, toggleWishlist } = useWishlist();
   const inWishlist = isInWishlist(pkg.id);
 
   return (
     <div className="package-card">
-      <img
-        src={pkg.image}
-        alt={pkg.name}
-        className="package-img"
-        loading="lazy"
-      />
+      <img src={pkg.image} alt={pkg.name} className="package-img" />
       <div className="package-info">
         <h3>{pkg.name}</h3>
         <p className="location">{pkg.location}</p>
@@ -26,20 +18,18 @@ export default function PackageCard({ package: pkg }) {
           <span className="duration">{pkg.duration}</span>
         </div>
 
-        <div className="rating-section">
-          <Rating rating={avgRating} reviewCount={reviewCount} />
+        <div className="package-actions">
+          <button
+            type="button"
+            className="wishlist-btn"
+            onClick={() => toggleWishlist(pkg.id)}
+          >
+            {inWishlist ? "Remove from Wishlist" : "Add to Wishlist"}
+          </button>
+          <Link to={`/package/${pkg.id}`} className="details-link">
+            View Details
+          </Link>
         </div>
-
-        <button
-          type="button"
-          className="wishlist-btn"
-          onClick={() => toggleWishlist(pkg.id)}
-        >
-          {inWishlist ? "Remove from Wishlist" : "Add to Wishlist"}
-        </button>
-        <Link to={`/package/${pkg.id}`} className="details-link">
-          View Details
-        </Link>
       </div>
     </div>
   );
